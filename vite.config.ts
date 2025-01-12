373aedd6e522688cb489c6a3a03f99324d5e35eb
desai <desai@atb-bremen.de>
2025-01-12 15:04:50 +0100

    Initial commit3

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sizzle-spice-hub-website/', // This is the repository name, change it if necessary
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
